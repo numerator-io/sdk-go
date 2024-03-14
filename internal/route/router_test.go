485a8dcd2bda4d50aaa635060520cfb102bd50fc
spartan-khanhvunguyen <khanh.vu.nguyen@c0x12c.com>
2024-03-14 10:25:10 +0700

    refactor current code

--- internal/route/router_test.go
+++ internal/route/router_test.go
@@ -53,7 +53,7 @@ func (s *RouterSuite) TearDownTest() {
 	s.TearDownSuite()
 }
 
-func Request(e *echo.Echo, method string, target string, queryParams map[string]string, bodyStr string) []byte {
+func Request(e *echo.Echo, method string, target string, bodyStr string) []byte {
 	var body io.Reader
 	if bodyStr != "" {
 		body = bytes.NewBufferString(bodyStr)
@@ -62,35 +62,28 @@ func Request(e *echo.Echo, method string, target string, queryParams map[string]
 	}
 	path := constant.BaseURL + target
 	connectRequest := httptest.NewRequest(method, path, body)
-	// Build query params
-	q := connectRequest.URL.Query()
-	for k, v := range queryParams {
-		q.Add(k, v)
-	}
-	connectRequest.URL.RawQuery = q.Encode()
 	connectRequest.Header.Add(echo.HeaderContentType, echo.MIMEApplicationJSON)
 	connectRequest.Header.Add(echo.HeaderAccept, echo.MIMEApplicationJSON)
 	connectRequest.Header.Add(constant.XNumAPIKeyHeader, constant.ApiKeyTest)
 
 	recorder := httptest.NewRecorder()
 	e.ServeHTTP(recorder, connectRequest)
-	fmt.Println(recorder.Body)
 
 	return recorder.Body.Bytes()
 }
 
-func RequestSuccess[T interface{}](e *echo.Echo, method string, target string, queryParams map[string]string, bodyStr string) T {
+func RequestSuccess[T interface{}](e *echo.Echo, method string, target string, bodyStr string) T {
 	var res response.SuccessResponse[T]
-	err := json.Unmarshal(Request(e, method, target, queryParams, bodyStr), &res)
+	err := json.Unmarshal(Request(e, method, target, bodyStr), &res)
 	if err != nil {
 		fmt.Println(err)
 	}
 	return res.SuccessResponse
 }
 
-func RequestFailure(e *echo.Echo, method string, target string, queryParams map[string]string, bodyStr string) response.FailureResponse {
+func RequestFailure(e *echo.Echo, method string, target string, bodyStr string) response.FailureResponse {
 	var res response.FailureResponse
-	err := json.Unmarshal(Request(e, method, target, queryParams, bodyStr), &res)
+	err := json.Unmarshal(Request(e, method, target, bodyStr), &res)
 	if err != nil {
 		fmt.Println(err)
 	}
